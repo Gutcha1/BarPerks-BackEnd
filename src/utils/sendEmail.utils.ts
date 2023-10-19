cb13955d494b1cd28d408cc159f310e42f89de64
Gustavo Barbalho <148292359+Gutcha1@users.noreply.github.com>
2023-10-19 20:52:01 -0300

    fix: links utils

--- src/utils/sendEmail.utils.ts
+++ src/utils/sendEmail.utils.ts
@@ -32,7 +32,7 @@ class EmailService {
             theme: 'default',
             product: {
                 name: 'Bar Perks',
-                link: `http://localhost:5173`
+                link: `https://barperks.vercel.app`
             }
         });
 
@@ -45,7 +45,7 @@ class EmailService {
                     button: {
                         color: '#22BC66',
                         text: 'Recuperar senha',
-                        link: `http://localhost:5173/recuperar-senha/cliente/${resetToken}`
+                        link: `https://barperks.vercel.app/recuperar-senha/cliente/${resetToken}`
                     }
                 },
                 outro: 'Necessita de ajuda ou tem dúvidas? Basta responder a este e-mail. Teremos todo o gosto em ajudar.'
@@ -66,7 +66,7 @@ class EmailService {
             theme: 'default',
             product: {
                 name: 'Bar Perks',
-                link: `http://localhost:5173`
+                link: `https://barperks.vercel.app/`
             }
         });
 
@@ -79,7 +79,7 @@ class EmailService {
                     button: {
                         color: '#22BC66',
                         text: 'Recuperar senha',
-                        link: `http://localhost:5173/recuperar-senha/estabelecimento/${resetToken}`
+                        link: `https://barperks.vercel.app/recuperar-senha/estabelecimento/${resetToken}`
                     }
                 },
                 outro: 'Necessita de ajuda ou tem dúvidas? Basta responder a este e-mail. Teremos todo o gosto em ajudar.'
