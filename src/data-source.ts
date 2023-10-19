2755ce168960c1da634626ca15bd558b6efd9718
Gustavo Barbalho <148292359+Gutcha1@users.noreply.github.com>
2023-10-18 22:47:30 -0300

    Update data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,6 +6,16 @@ import { DataSource, DataSourceOptions } from 'typeorm'
 const dataSourceConfig = (): DataSourceOptions => {
     const entitiesPath: string = path.join(__dirname, './entities/**.{ts,js}')
     const migrationsPath: string = path.join(__dirname, './migrations/**.{ts,js}')
+    const nodeEnv: string = process.env.NODE_ENV;
+
+    if (nodeEnv === "production") {
+      return {
+        type: "postgres",
+        url: process.env.DATABASE_URL,
+        entities: [entitiesPath],
+        migrations: [migrationsPath],
+      };
+    }
 
     const dbUrl: string | undefined = process.env.DATABASE_URL
 
