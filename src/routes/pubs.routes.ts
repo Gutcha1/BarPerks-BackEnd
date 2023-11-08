d4ac0509a12bc22566b774a020200f56b19aed3b
Gustavo Barbalho <148292359+Gutcha1@users.noreply.github.com>
2023-11-08 17:45:38 -0300

    fix: testign message

--- src/routes/pubs.routes.ts
+++ src/routes/pubs.routes.ts
@@ -25,49 +25,6 @@ pubsRoutes.post('/plan', async (req: Request, res: Response): Promise<void> => {
     const token = "APP_USR-2600481674697355-110617-ebde29dc7f6bbd01fd4beaffdc12f070-74670153"
 
     return 'ok'
-
-    //if(data.data.id){
-    //    await fetch(`https://api.mercadopago.com/v1/payments/${data.data.id}`, {
-    //        method: 'GET',
-    //        headers: {
-    //            'Content-Type': 'application/json',
-    //            'Authorization': `Bearer ${token}`
-    //        }
-    //    }).then(reponse => reponse.json())
-    //    .then(async (responseJson) => {
-
-    //        if(responseJson.status == "approved"){
-    //            const planRepository: Repository<Plan> = AppDataSource.getRepository(Plan);
-
-    //            const findPlan: Plan | null = await planRepository.findOne({
-    //                where: {
-    //                    pub: {
-    //                        email: responseJson.additional_info.items[0].id
-    //                    }
-    //                },
-    //                relations: {
-    //                    pub: true
-    //                }
-    //            });
-
-    //            if (!findPlan) {
-    //                throw new AppError('Plano não encontrado', 404);
-    //            }
-
-    //            const newDataPlan = {
-    //                ...findPlan,
-    //                name: responseJson.description
-    //            };
-    //        
-    //            await planRepository.save(newDataPlan);
-    //            
-    //            const plan = plansSchemaResponse.parse(newDataPlan);
-    //            
-    //            return plan;
-    //        }
-    //    })
-    //    .catch(erro => console.log(erro))
-    }
 })
 
 export default pubsRoutes
