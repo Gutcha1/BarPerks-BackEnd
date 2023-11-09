02264904a92f7bde522ce34cce108c412ee04586
Gustavo Barbalho <148292359+Gutcha1@users.noreply.github.com>
2023-11-09 18:18:10 -0300

    fix: create plan

--- src/routes/pubs.routes.ts
+++ src/routes/pubs.routes.ts
@@ -20,8 +20,7 @@ pubsRoutes.delete('/:id', ensureAuthIsValidMiddleware, ensurePubAccount, deleteP
 pubsRoutes.post('/recuperar-senha', sendEmailResetPasswordController)
 pubsRoutes.patch('/recuperar-senha/:token', resetPasswordController)
 pubsRoutes.patch('/upload/:id', (upload.single('file')), uploadPubController)
-pubsRoutes.post('/plan', async (req: Request, res: Response): Promise<Response> => {
-
+pubsRoutes.post('/plan', async (req: Request, res: Response): Promise<void> => {
     const data = req.body
     const token = "APP_USR-2600481674697355-110617-ebde29dc7f6bbd01fd4beaffdc12f070-74670153"
 
